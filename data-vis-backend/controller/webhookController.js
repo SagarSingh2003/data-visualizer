65fd47340809050df325bb237d1cae766e1845e6
SagarSingh2003 <iamsagar762@gmail.com>
2024-12-03 20:51:14 +0530

    model add

--- data-vis-backend/controller/webhookController.js
+++ data-vis-backend/controller/webhookController.js
@@ -1,9 +1,13 @@
 import ApiResponse from "../utils/ApiReponses.js";
+import dbService from "../utils/dbService.js";
+import dataModel from "../model/chartData.js";
 
 const webhookController = {
 
-    updateData : (req , res) => {
+    updateData : async (req , res) => {
         console.log(req.body)
+
+        console.log(await dbService.find(dataModel))
         return new ApiResponse(res).successful()
     }
 }
---
+++ data-vis-backend/model/chartData.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const dataSchema = new mongoose.Schema({
+    day: {
+        type: Date,
+        required: true
+    },
+    age: {
+        type: String,
+        enum: ['15-25', '>25'],
+        required: true
+    },
+    gender: {
+        type: String,
+        enum: ['Male', 'Female'],
+        required: true
+    },
+    A: {
+        type: Number,
+        required: true
+    },
+    B: {
+        type: Number,
+        required: true
+    },
+    C: {
+        type: Number,
+        required: true
+    },
+    D: {
+        type: Number,
+        required: true
+    },
+    E: {
+        type: Number,
+        required: true
+    },
+    F: {
+        type: Number,
+        required: true
+    }
+});
+
+export default dataModel = mongoose.model('DataRecord', dataSchema);
